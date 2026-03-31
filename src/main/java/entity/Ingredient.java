6546facbb97517cfab3a9bcd635addf515c96819
Antenaina012 <hei.antenaina.7@gmail.com>
2026-03-31 13:35:31 +0000

    fixed getConnection error

--- src/main/java/entity/Ingredient.java
+++ src/main/java/entity/Ingredient.java
@@ -1,18 +1,17 @@
 package entity;
 
 public class Ingredient {
-        private int id;
-            private String name;
-                private String unit;
+    private int id;
+    private String name;
+    private String unit;
 
-                    public Ingredient(int id, String name, String unit) {
-                            this.id = id;
-                                    this.name = name;
-                                            this.unit = unit;
-                                                }
+    public Ingredient(int id, String name, String unit) {
+        this.id = id;
+        this.name = name;
+        this.unit = unit;
+    }
 
-                                                    public int getId() { return id; }
-                                                        public String getName() { return name; }
-                                                            public String getUnit() { return unit; }
-                                                            }
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getUnit() { return unit; }
 }
--- src/main/java/repository/IngredientRepository.java
+++ src/main/java/repository/IngredientRepository.java
@@ -6,14 +6,20 @@ import java.sql.*;
 import java.util.*;
 
 public class IngredientRepository {
+    private DataSource dataSource;
+
+    public IngredientRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     public List<Ingredient> findAll() throws SQLException {
         List<Ingredient> ingredients = new ArrayList<>();
         String sql = "SELECT * FROM ingredient";
-        try (Connection conn = DataSource.getConnection();
+        try (Connection conn = dataSource.getConnection();
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
             while (rs.next()) {
-                ingredients.add(new Ingredient(rs.getInt("id"), rs.getString("name"), rs.getDouble("unit_price")));
+                ingredients.add(new Ingredient(rs.getInt("id"), rs.getString("name"), rs.getString("unit")));
             }
         }
         return ingredients;
